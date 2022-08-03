f17755f9f05612250f01bb5f55c80801920f32c9
definejae234 <definejae234@gmail.com>
2022-08-03 17:49:38 +0900

    reference

--- reference.cpp
+++ reference.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main()
 {
 	int num = 10;
-	int& ref = num;
+	int& ref = num;//레퍼런스 변수를 사용할 시는 선언과 동시에 초기화를 해야함.
+
+
 
 	cout << "num = " << num << endl;
 	cout << "ref = " << ref << endl;
@@ -17,6 +19,14 @@ int main()
 
 	cout << "&num = " << &num << endl;
 	cout << "&ref = " << &ref << endl;
+
+	int num2, ref2;
+
+	num2 = ref2 = 100;
+
+	cout << num2 << "," << ref2 << endl;
+	cout << &num2 << "," << &ref2 << endl;
+	//레퍼런스 변수를 선언할 때는 데이터형이 일치하여야 한다.
 
 	return 0;
 }
